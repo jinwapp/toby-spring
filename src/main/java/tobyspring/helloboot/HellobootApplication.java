907c4425af8b6eb3edd4ea97c768cb7226151745
Jay <cobosys@MacBook-Pro.local>
2023-05-23 19:44:36 +0900

    자바코드 구성정보 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -21,14 +25,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Configuration
 public class HellobootApplication {
 
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
+
 	public static void main(String[] args) {
-
 		// 스프링 컨테이너 생성 applicationContext
-
-
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -39,14 +51,13 @@ public class HellobootApplication {
 				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 				WebServer webserver = serverFactory.getWebServer(servletContext -> {
 					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this) // 스프링을 위한 서블릿 : DispactcherServlet
-					).addMapping("/*"); // front controller
+							new DispatcherServlet(this)) // 스프링을 위한 서블릿 : DispactcherServlet
+					.addMapping("/*"); // front controller
 				});
 				webserver.start();
 			}
 		}; // 스프링 컨테이너 : applicationContext
-		applicationContext.registerBean(HelloController.class); // 빈 등록
-		applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
+		applicationContext.register(HellobootApplication.class); // 빈 등록
 		applicationContext.refresh(); // 빈 생성
 	}
 }
