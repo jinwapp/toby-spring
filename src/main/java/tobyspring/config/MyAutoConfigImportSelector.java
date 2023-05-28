dd2f613dc80e21507e3b0e468a5e06ed414c5f7a
jinwapp <jinwapp@gmail.com>
2023-05-28 20:21:59 +0900

    ㄴㅇㄻㄴㅇ

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,27 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "tobyspring.config.autoconfig.TomcatWebServerConfig",
-                "tobyspring.config.autoconfig.DispatcherServletConfig"
-        };
+        List<String> autoconfigs = new ArrayList<>();
+
+        for (String candidate : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+            autoconfigs.add(candidate);
+        }
+        return autoconfigs.toArray(new String[0]);
     }
 }
